b97603bc516fdf644bc2cdabd6585711fd996e87
vishnu-vvk <99391852+vishnu-vvk@users.noreply.github.com>
2022-02-10 19:26:02 +0530

    Added
    Applied

---
+++ src/main/scala/util/ValidateConversion.scala
@@ -0,0 +1,37 @@
+package util
+
+import mapper.UnitMap
+
+class ValidateConversion {
+
+  var map = new UnitMap
+
+  def checkTens(ip:String):String={
+    if((ip(1)+""+ip(0)).toInt<20)
+      return map.mapUnits(ip(1) + "" + ip(0))
+    else
+      return map.mapTens(ip(1).toString)+" "+ map.mapUnits(ip(0).toString)
+  }
+
+  def checkHundreds(ip:String,i:Int):String={
+    if(ip.toInt==0)
+      return ""
+    else if(i!=0)
+      return map.mapUnits(ip(2).toString)+" hundred"
+    else {
+      if((ip(1)+""+ip(0)).toInt==0)
+        return map.mapUnits(ip(2).toString)+" hundred"
+      else
+        return map.mapUnits(ip(2).toString)+" hundred and "
+    }
+  }
+
+  def checkPlaces(ip:String,i:Int):String={
+    if(ip.toInt==0)
+      return ""
+    else
+      return " "+map.arrayPlace(i.toString)+" "
+  }
+
+
+}
